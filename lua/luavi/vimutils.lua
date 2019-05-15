2193879a0b6f57477a6ba518779dc2538983cded
Shidong Wang <wsdjeg@outlook.com>
2019-05-15 09:50:15 +0800

    Fix vim support

--- lua/luavi/vimutils.lua
+++ lua/luavi/vimutils.lua
@@ -1,29 +1,55 @@
 vimutils = {}
- 
+
 vimutils.constant = "hello"
 
+function table.has_key(table, key)
+    for key, value in pairs(table) do
+        if key == key then
+            return true
+        end
+    end
+    return false
+end
+
+local is_nvim = table.has_key(vim, 'api')
 
 function vimutils.command(cmd)
-    vim.api.nvim_command(cmd)
+    if is_nvim then
+        vim.api.nvim_command(cmd)
+    else
+        vim.command(cmd)
+    end
 end
 
 
 function vimutils.eval(expr)
-    return vim.api.nvim_eval(expr)
+    if is_nvim then
+        return vim.api.nvim_eval(expr)
+    else
+        return vim.eval(expr)
+    end
 end
 
 
 function vimutils.get_current_line()
-    return vim.api.nvim_get_current_line()
+    if is_nvim then
+        return vim.api.nvim_get_current_line()
+    else
+        return vim.window().line
+    end
 end
 
 
 function vimutils.current_buffer()
-    return vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), 0, -1, 0)
+    if is_nvim then
+        return vim.api.nvim_buf_get_lines(vim.api.nvim_get_current_buf(), 0, -1, 0)
+    else
+        return vim.window().buffer
+    end
 end
 
 function vimutils.current_linenr()
-    return vim.api.nvim_eval('line(".")')
+    return vimutils.eval('line(".")')
 end
 
 return vimutils
